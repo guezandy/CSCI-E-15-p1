fb265d93dce041e14b26abfbaa9ce12f9acb7246
Andrew Rodriguez <andrewr@twitter.com>
2018-09-14 17:18:42 -0700

    Just missing the quotes

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 Start README.md
 
 Project 1
+
+External resources:
+Centering with flexbox
+https://stackoverflow.com/questions/19026884/flexbox-center-horizontally-and-vertically
--- index.php
+++ index.php
@@ -1,17 +1,23 @@
 <!doctype html>
 <html>
 <head>
-
-	<title>Hello World</title>
+	<title>Andrew Rodriguez</title>
 	<meta charset='utf-8'>
-		
+	<link href="css/styles.css" rel="stylesheet">
 </head>
 <body>
-
-	<?php 
-	echo 'Hello World! You are visiting ';
-	echo $_SERVER['SERVER_NAME']; 
-	?>
-	
+	<div class="flex-container">
+		<div class="row"> 
+			<div>
+				<h1>Andrew Rodriguez</div>
+			<div class="flex-item">
+				<img src="images/drew.jpg">
+			</div>
+			<div>
+				<h4>Cubanito de Miami - living now in San Francisco. Like writing code to make things move.</h4>
+			</div>
+			<div>Quote</div>
+		</div>
+	</div>
 </body>
 </html>
